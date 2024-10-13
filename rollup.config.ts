1c9d7fc959b70a3734abe6245635440a0eec2546
Andreas Stassivik <andreas@stassi.net>
2024-10-13 03:48:05 -0700

    `quick-start.html` and its script bundle generated by `@rollup/plugin-html` template (missing stylesheets embedded in the original)

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,22 @@
-// noinspection JSUnusedGlobalSymbols
+// noinspection HtmlRequiredLangAttribute, JSUnusedGlobalSymbols
 
+import {
+  type OutputAsset,
+  type OutputChunk,
+  type Plugin,
+  type RollupOptions,
+} from 'rollup'
 import commonjs from '@rollup/plugin-commonjs'
+import html, {
+  makeHtmlAttributes,
+  type RollupHtmlTemplateOptions,
+} from '@rollup/plugin-html'
 import inject from '@rollup/plugin-inject'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 // @ts-expect-error -- untyped plugin
 import untypedModify from 'rollup-plugin-modify'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { type Plugin, type RollupOptions } from 'rollup'
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- untyped plugin
 const modify: (modifyOptions: {
@@ -48,14 +57,59 @@ const rollupConfig: RollupOptions[] = [
     external: ['@stassi/leaf'],
     input: 'src/tutorial/quick-start/quick-start.ts',
     output: {
-      file: 'public/tutorial/quick-start/dist/script/quick-start.js',
+      dir: 'public/tutorial/quick-start/dist/',
+      entryFileNames: 'script/quick-start.js',
       format: 'esm',
       paths: {
         '@stassi/leaf': '../../../../leaf/leaf.js',
       },
       sourcemap: true,
     },
-    plugins: [typescript(), terser()],
+    plugins: [
+      typescript(),
+      terser(),
+      html({
+        fileName: 'quick-start.html',
+        template({
+          attributes,
+          files,
+          meta,
+          publicPath,
+          title,
+        }: RollupHtmlTemplateOptions): string {
+          return `
+            <!DOCTYPE html>
+            <html${makeHtmlAttributes(<Record<string, string>>attributes.html)}>
+            <head>
+              ${meta
+                .map(
+                  (input: Record<string, string>): string =>
+                    `<meta${makeHtmlAttributes(input)}>`,
+                )
+                .join('\n')}
+              <title>${title}</title>
+              ${(files.css ?? [])
+                .map(
+                  ({ fileName }: OutputChunk | OutputAsset): string =>
+                    `<link href="${publicPath}${fileName}" rel="stylesheet"${makeHtmlAttributes(<Record<string, string>>attributes.link)}>`,
+                )
+                .join('\n')}
+            </head>
+            <body>
+              <div id="map"></div>
+              ${(files.js ?? [])
+                .map(
+                  ({ fileName }: OutputChunk | OutputAsset): string =>
+                    `<script src="${publicPath}${fileName}"${makeHtmlAttributes(<Record<string, string>>attributes.script)}></script>`,
+                )
+                .join('\n')}
+            </body>
+            </html>
+          `
+        },
+        title: 'Leaflet Quick Start Guide',
+      }),
+    ],
   },
   {
     external: ['@stassi/leaf'],
