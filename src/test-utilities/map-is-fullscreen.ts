ec16f140ef002da7a65ee9cff6808ca7330c9f9d
Andreas Stassivik <andreas@stassi.net>
2024-10-27 15:03:14 -0700

    remove superfluous test utilities

--- src/test-utilities/map-is-fullscreen.ts
+++
@@ -1,5 +0,0 @@
-export function mapIsFullscreen(): Promise<boolean> {
-  return page.$eval('#map', (element: Element): boolean =>
-    element.classList.contains('leaflet-fullscreen-on'),
-  )
-}
--- src/test-utilities/wait-until-map-class/fullscreen-on.ts
+++
@@ -1,5 +0,0 @@
-import { type ElementHandle } from 'puppeteer'
-
-export function waitUntilMapClassFullscreenOn(): Promise<ElementHandle | null> {
-  return page.waitForSelector('#map.leaflet-fullscreen-on')
-}
