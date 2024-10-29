cf9dfecacca3cf9f6311163e48d392ac30e1464c
Andreas Stassivik <andreas@stassi.net>
2024-10-28 21:20:33 -0700

    interactive accessibility tutorial typescript implementation

---
+++ src/tutorial/accessibility/interactive.ts
@@ -0,0 +1,35 @@
+import 'leaflet/dist/leaflet.css'
+import '../style/theme.css'
+import '../style/medium.css'
+
+import { type Map } from '@stassi/leaf'
+
+const altText = 'Kyiv',
+  map: Map = await (
+    await import('../../leaf/map/map.js')
+  ).map({
+    center: [50.4501, 30.5234],
+    id: 'map',
+    zoom: 4,
+  })
+
+await (
+  await import('../../leaf/marker.js')
+).marker({
+  altText,
+  iconOptions: {
+    iconUrl: '../../../leaflet/images/marker-icon.png',
+    iconUrlRetina: '../../../leaflet/images/marker-icon-2x.png',
+    shadowUrl: '../../../leaflet/images/marker-shadow.png',
+  },
+  latitudeLongitude: [50.4501, 30.5234],
+  map,
+  popupContent: `${altText}, Ukraine is the birthplace of Leaflet!`,
+})
+
+await (
+  await import('../../leaf/tile-layer/tile-layer-osm.js')
+).tileLayerOsm({
+  map,
+  zoomMax: 19,
+})
