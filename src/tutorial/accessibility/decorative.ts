ae6ffcebaef556028670ffef7b9a02978a5d79da
Andreas Stassivik <andreas@stassi.net>
2024-11-03 05:03:53 -0800

    dynamically set `inert` attribute on decorative map

--- src/tutorial/accessibility/decorative.ts
+++ src/tutorial/accessibility/decorative.ts
@@ -4,6 +4,8 @@ import '../style/medium.css'
 
 import { type Map } from '@stassi/leaf'
 
+document.getElementById('map')?.setAttribute('inert', '')
+
 const map: Map = await (
   await import('../../leaf/map/map.js')
 ).map({
