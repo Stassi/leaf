c1f6b71105da0e8c56f0700eda0fc3741732beb8
Andreas Stassivik <andreas@stassi.net>
2024-11-03 04:45:03 -0800

    decorative accessibility tutorial TypeScript conversion

---
+++ src/tutorial/accessibility/decorative.ts
@@ -0,0 +1,26 @@
+import 'leaflet/dist/leaflet.css'
+import '../style/theme.css'
+import '../style/medium.css'
+
+import { type Map } from '@stassi/leaf'
+
+const map: Map = await (
+  await import('../../leaf/map/map.js')
+).map({
+  center: [50.4501, 30.5234],
+  id: 'map',
+  zoom: 4,
+})
+
+await (
+  await import('../../leaf/marker.js')
+).marker({
+  latitudeLongitude: [50.4501, 30.5234],
+  map,
+})
+
+await (
+  await import('../../leaf/tile-layer/tile-layer-osm.js')
+).tileLayerOsm({
+  map,
+})
