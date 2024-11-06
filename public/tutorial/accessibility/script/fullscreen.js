a34731859871596886f5c294c0d35081eba723b3
Andreas Stassivik <andreas@stassi.net>
2024-11-06 12:46:37 -0800

    `MapFullscreen` integration in fullscreen accessibility tutorial

--- public/tutorial/accessibility/script/fullscreen.js
+++ public/tutorial/accessibility/script/fullscreen.js
@@ -1,10 +1,17 @@
-import { createFullscreenMap } from '../../../script/leaflet-fullscreen.js'
+import { attributionOsm, urlTemplateOsm } from '../../../script/base-layers.js'
 import { marker } from '../../../leaflet-adapter/marker.js'
 import { tileLayer } from '../../../leaflet-adapter/tile-layer/tile-layer.js'
-import { attributionOsm, urlTemplateOsm } from '../../../script/base-layers.js'
+import { MapFullscreen } from '../../../script/leaflet-fullscreen.js'
+
+function mapFullscreen({ id, ...props }) {
+  return new MapFullscreen(id, {
+    fullscreenControl: true,
+    ...props,
+  })
+}
 
 const altText = 'Kyiv',
-  map = createFullscreenMap({
+  map = mapFullscreen({
     center: [50.4501, 30.5234],
     id: 'map',
     zoom: 4,
