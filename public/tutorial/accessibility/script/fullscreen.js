2aff1125cf3bcf8c2cd931b244e4e35e1d09d36d
Andreas Stassivik <andreas@stassi.net>
2024-11-12 19:43:28 -0800

    cascading property destructuring integration (unary interface)

--- public/tutorial/accessibility/script/fullscreen.js
+++ public/tutorial/accessibility/script/fullscreen.js
@@ -3,15 +3,8 @@ import { marker } from '../../../leaflet-adapter/marker.js'
 import { tileLayer } from '../../../leaflet-adapter/tile-layer/tile-layer.js'
 import { fullscreenMap } from '../../../script/leaflet-fullscreen.js'
 
-function mapFullscreen({ id, ...props }) {
-  return fullscreenMap(id, {
-    fullscreenControl: true,
-    ...props,
-  })
-}
-
 const altText = 'Kyiv',
-  map = mapFullscreen({
+  map = fullscreenMap({
     center: [50.4501, 30.5234],
     id: 'map',
     zoom: 4,
