4040f07bb07610b51d3b530f3f9ca191cdb79a5f
Andreas Stassivik <andreas@stassi.net>
2024-11-15 14:22:07 -0800

    `fullscreen.state.get: UseBooleanGet` replaces `<boolean>fullscreen` & export `SetControlAnchorTitleOptions` subtypes: - AnchorAssignTitleOptions - ControlAnchorTitleStates

--- src/leaf/map/fullscreen/control/set-control-anchor-title.ts
+++ src/leaf/map/fullscreen/control/set-control-anchor-title.ts
@@ -1,23 +1,27 @@
-import { type UseAnchor } from '../state/use-anchor'
+import { type AnchorAssign } from '../state/use-anchor'
+import { type UseBooleanGet } from '../state/use-boolean'
+
+export type ControlAnchorTitleStates = Record<'false' | 'true', string>
+export type AnchorAssignTitleOptions = {
+  assign: AnchorAssign
+  titleStates: ControlAnchorTitleStates
+}
 
 export type SetControlAnchorTitleOptions = {
-  anchor: {
-    assign: UseAnchor['assign']
-    titleStates: Record<'false' | 'true', string>
-  }
-  fullscreen: boolean
+  anchor: AnchorAssignTitleOptions
+  fullscreen: { state: { get: UseBooleanGet } }
 }
 
 export function setControlAnchorTitle({
   anchor: { assign: anchorAssign, titleStates: anchorTitleStates },
-  fullscreen,
+  fullscreen: {
+    state: { get: getFullscreenState },
+  },
 }: SetControlAnchorTitleOptions): HTMLElement {
   return anchorAssign({
     title:
       anchorTitleStates[
-        <keyof SetControlAnchorTitleOptions['anchor']['titleStates']>(
-          fullscreen.toString()
-        )
+        <keyof ControlAnchorTitleStates>getFullscreenState().toString()
       ],
   })
 }
