c02bc7fbf15cfeeee8ea63f255ba677304bc27b8
Andreas Stassivik <andreas@stassi.net>
2024-11-17 01:27:26 -0800

    `control` implementation

---
+++ src/leaf/control.ts
@@ -0,0 +1,32 @@
+import {
+  type ControlOptions as LeafletControlOptions,
+  control as untypedLeafletControl,
+} from 'leaflet'
+
+import { type Control, type ControlPosition, type Map } from '@stassi/leaf'
+
+export type ControlOnAdd = (map: Map) => HTMLElement
+export type ControlOptions = LeafletControlOptions &
+  Partial<{
+    map: Map
+    onAdd: ControlOnAdd
+    position: ControlPosition
+  }>
+
+export function control({
+  map,
+  onAdd,
+  position = 'topright',
+  ...props
+}: ControlOptions): Control {
+  const created: Control = (<(options: LeafletControlOptions) => Control>(
+    (<unknown>untypedLeafletControl)
+  ))({
+    position,
+    ...props,
+  })
+
+  created.onAdd = onAdd ? onAdd : undefined
+
+  return map ? created.addTo(map) : created
+}
